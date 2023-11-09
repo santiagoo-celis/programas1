3f804017539d614108e640d16e82a6526ae31190
santiagoo-celis <147632812+santiagoo-celis@users.noreply.github.com>
2023-11-09 10:43:52 -0500

    Update programa 5.py

--- programa 5.py
+++ programa 5.py
@@ -1,3 +1,6 @@
 #Programa para verificar si un número es par o impar
 numero = int(input("Ingrese un número: "))
-print("Es un número par." if numero % 2 == 0 else "Es un número impar.")
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("Es un número impar.")
