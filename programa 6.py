bdfca83d1ed8fbe71cc4f03d171a3edd3db8a7ec
santiagoo-celis <147632812+santiagoo-celis@users.noreply.github.com>
2023-11-09 11:01:26 -0500

    Update programa 6.py

--- programa 6.py
+++ programa 6.py
@@ -1,4 +1,12 @@
-#Escriba un programa que dado un número imprima la suma de sus dígitos
-numero = input("Ingrese un número: ")
-suma_digitos = sum(int(digito) for digito in numero if digito.isdigit())
-print("La suma de los dígitos es:", suma_digitos)
+#Programa para encontrar el máximo entre dos números.
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
+# Compara los tres números
+if numero1 > numero2:
+    maximo = numero1
+if numero2 > numero3:
+    maximo = numero2
+else:
+    maximo = numero3
+print("El número máximo es:", maximo)
